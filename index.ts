fdc7241e03cc981008dcdc3e7d59da228c3fda46
Scott Milner <scottdmilner@gmail.com>
2023-03-01 21:13:50 -0700

    start to say what happened

--- index.ts
+++ index.ts
@@ -16,17 +16,28 @@ app.get('/accomplice/discord', async (req: Request, res: Response) => {
 app.post('/accomplice/discord', async (req: Request, res: Response) => {
 	console.log(req.body);
 	
+	const message = `
+	**A thing has happened!** :confetti_ball:\n 
+	There was a \`${req.body.meta.type}\` on the entity \`${req.body.entity.id}\`!
+	Have a great day :relaxed:
+	`
+
 	fetch(process.env.DISCORD_HOOK_URL as string, {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify({
-			username: 'I\'m a bot',
-			content: "hello there",
+			username: 'ShBotGrid',
+			content: message,
+			avatar_url: 'http://animhooks.cs.byu.edu/img/shotgriscord.png',
 		}),
 	})
 	.then(r => res.sendStatus(200));
 });
 
+
+app.get('/img/shotgriscord.png', (req: Request, res: Response) => {
+	res.sendFile('img/shotgriscord.png');
+})
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
