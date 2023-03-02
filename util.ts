5760a26bf405db65640fa6f7171e15d98ee364c0
Scott Milner <scottdmilner@gmail.com>
2023-03-02 09:58:16 -0700

    add logging

--- util.ts
+++ util.ts
@@ -2,12 +2,16 @@ import { Request } from 'express';
 import crypto, { sign } from 'crypto';
 
 export function verifyMessage(req: Request, secretToken: string): boolean {
-    const shas1Hash = 'sha1=' + crypto.createHash('sha1')
+    const sha1Hash = 'sha1=' + crypto.createHash('sha1')
         .update(secretToken)
         .update(JSON.stringify(req.body))
         .digest('hex');
     
-    const signature = req.headers['x-sg-signature'];
+    const signature = req.headers['x-sg-signature'] as string;
 
-    return shas1Hash == signature;
+    // debug
+    console.log(sha1Hash);
+    console.log(signature);
+
+    return sha1Hash == signature;
 }
