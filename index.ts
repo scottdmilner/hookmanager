b1324e744c29b4ca17699eb632dbe7a5a9cb48e8
Scott Milner <scottdmilner@gmail.com>
2023-03-02 10:08:15 -0700

    standardize indentation to spaces

--- index.ts
+++ index.ts
@@ -10,36 +10,36 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.get('/accomplice/discord', async (req: Request, res: Response) => {
-	console.log('Hello there');
-	res.sendStatus(200);
+    console.log('Hello there');
+    res.sendStatus(200);
 });
 
 app.post('/accomplice/discord', (req: Request, res: Response) => {
-	console.log(req.body);
+    console.log(req.body);
 
-	if (!verifyMessage(req, process.env.SECRET_TOKEN as string)) {
-		console.log('Bad request!');
-		res.sendStatus(401);
-		return;
-	}
-	
-	const message = `
-	**A thing has happened!** :confetti_ball:
+    if (!verifyMessage(req, process.env.SECRET_TOKEN as string)) {
+        console.log('Bad request!');
+        res.sendStatus(401);
+        return;
+    }
+    
+    const message = `
+    **A thing has happened!** :confetti_ball:
 There was a \`${req.body.data.meta.type}\` on the entity \`${req.body.data.entity.id}\`!
 Have a great day :relaxed:`
 
-	fetch(process.env.DISCORD_HOOK_URL as string, {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({
-			username: 'ShBotGrid',
-			content: message,
-		}),
-	})
-	.then((fetchResponse) => {
-		if (fetchResponse.ok) res.sendStatus(200);
-		else                  res.sendStatus(500);
-	});
+    fetch(process.env.DISCORD_HOOK_URL as string, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            username: 'ShBotGrid',
+            content: message,
+        }),
+    })
+    .then((fetchResponse) => {
+        if (fetchResponse.ok) res.sendStatus(200);
+        else                  res.sendStatus(500);
+    });
 });
 
 app.listen(port, () => {
